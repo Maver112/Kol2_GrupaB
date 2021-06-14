9aa69251f67ef3b5ed85e34e2445d233a66957f3
Grubicio <wiktor.ziaja1@gmail.com>
2021-06-14 08:48:13 +0200

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {WZForumComponent} from './components/wz-forum/wz-forum.component';
+import {WZForumDetailsComponent} from './components/wz-forum-details/wz-forum-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: WZForumComponent
+  },
+  {
+    path: 'forum/detail/:id',
+    component: WZForumDetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WZForumComponent } from './components/wz-forum/wz-forum.component';
+import { WZForumItemComponent } from './components/wz-forum-item/wz-forum-item.component';
+import { WZForumDetailsComponent } from './components/wz-forum-details/wz-forum-details.component';
+import {HttpClientModule} from '@angular/common/http';
+import {WZDataService} from './wz-data.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WZForumComponent,
+    WZForumItemComponent,
+    WZForumDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [WZDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
